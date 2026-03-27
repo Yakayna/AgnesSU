21b3beed68018819d7cfa63a92b6e15a27143722
YC酱luyancib <luyancib@qq.com>
2026-03-27 20:08:11 +0800

    kernel: commonize

--- kernel/inline_hook_check.mk
+++ kernel/inline_hook_check.mk
@@ -1,48 +1,17 @@
-  ifeq ($(shell grep -q "ksu_handle_setresuid" $(srctree)/kernel/sys.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/susfs_inline: ksu_handle_setresuid found)
-  else
-    $(info -- ERROR: Missing ksu_handle_setresuid hook in kernel/sys.c)
-    $(error Please integrate KernelSU hooks first: https://kernelsu.org/guide/how-to-integrate-for-non-gki.html)
-  endif
+define check_ksu_hook
+    ifeq ($$(shell grep -q "$(1)" $(2); echo $$$$?),0)
+        $$(info -- $$(REPO_NAME)/susfs_inline: $(1) found)
+    else
+        $$(info -- You lost $(1) hook in your kernel)
+        $$(info -- Please submit issue to your SUSFS patches author.)
+        $$(error You should integrate $$(REPO_NAME) in your kernel. $(3))
+    endif
+endef
 
-  ifeq ($(shell grep -q "ksu_handle_execveat" $(srctree)/fs/exec.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/susfs_inline: ksu_handle_execveat found)
-  else
-    $(info -- ERROR: Missing ksu_handle_execveat hook in fs/exec.c)
-    $(error Please integrate KernelSU hooks first)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_faccessat" $(srctree)/fs/open.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/susfs_inline: ksu_handle_faccessat found)
-  else
-    $(info -- ERROR: Missing ksu_handle_faccessat hook in fs/open.c)
-    $(error Please integrate KernelSU hooks first)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_stat" $(srctree)/fs/stat.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/susfs_inline: ksu_handle_stat found)
-  else
-    $(info -- ERROR: Missing ksu_handle_stat hook in fs/stat.c)
-    $(error Please integrate KernelSU hooks first)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_sys_reboot" $(srctree)/kernel/reboot.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/susfs_inline: ksu_handle_sys_reboot found)
-  else
-    $(info -- ERROR: Missing ksu_handle_sys_reboot hook in kernel/reboot.c)
-    $(error Please integrate KernelSU hooks first)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_input_handle_event" $(srctree)/drivers/input/input.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/susfs_inline: ksu_handle_input_handle_event found)
-  else
-    $(info -- ERROR: Missing ksu_handle_input_handle_event hook in drivers/input/input.c)
-    $(error Please integrate KernelSU hooks first)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_devpts" $(srctree)/fs/devpts/inode.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/susfs_inline: ksu_handle_devpts found)
-  else
-    $(info -- ERROR: Missing ksu_handle_devpts hook in fs/devpts/inode.c)
-    $(error Please integrate KernelSU hooks first)
-  endif
+  $(eval $(call check_ksu_hook,ksu_handle_setresuid,$(srctree)/kernel/sys.c))
+  $(eval $(call check_ksu_hook,ksu_handle_execveat,$(srctree)/fs/exec.c))
+  $(eval $(call check_ksu_hook,ksu_handle_faccessat,$(srctree)/fs/open.c))
+  $(eval $(call check_ksu_hook,ksu_handle_stat,$(srctree)/fs/stat.c))
+  $(eval $(call check_ksu_hook,ksu_handle_sys_reboot,$(srctree)/kernel/reboot.c))
+  $(eval $(call check_ksu_hook,ksu_handle_input_handle_event,$(srctree)/drivers/input/input.c))
+  $(eval $(call check_ksu_hook,ksu_handle_devpts,$(srctree)/fs/devpts/inode.c))
--- kernel/manual_hook_check.mk
+++ kernel/manual_hook_check.mk
@@ -1,3 +1,14 @@
+define check_ksu_hook
+    ifeq ($$(shell grep -q "$(1)" $(2); echo $$$$?),0)
+        $$(info -- $$(REPO_NAME)/manual_hook: $(1) found)
+    else
+        $$(info -- You lost $(1) hook in your kernel)
+        $$(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
+        $$(error You should integrate $$(REPO_NAME) in your kernel. $(3))
+    endif
+endef
+
+
   ifeq ($(CONFIG_KSU_MANUAL_HOOK_AUTO_SETUID_HOOK), y)
     $(info -- $(REPO_NAME)/manual_hook: You are using LSM hooks for setuid hooks.)
     ifeq ($(VERSION),6)
@@ -11,13 +22,7 @@
   else
     $(info -- $(REPO_NAME)/manual_hook: You are using a manual setresuid hook for setuid hooks.)
 
-    ifeq ($(shell grep -q "ksu_handle_setresuid" $(srctree)/kernel/sys.c; echo $$?),0)
-      $(info -- $(REPO_NAME)/manual_hook: ksu_handle_setresuid found)
-    else
-      $(info -- You lost ksu_handle_setresuid hook in your kernel)
-      $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-      $(error You should integrate $(REPO_NAME) in your kernel.)
-    endif
+    $(eval $(call check_ksu_hook,ksu_handle_setresuid,$(srctree)/kernel/sys.c))
   endif
 
   ifeq ($(CONFIG_KSU_MANUAL_HOOK_AUTO_INITRC_HOOK), y)
@@ -33,13 +38,8 @@
   else
     $(info -- $(REPO_NAME)/manual_hook: You are using a manual sys_read hook for init rc hooks.)
 
-    ifeq ($(shell grep -q "ksu_handle_sys_read" $(srctree)/fs/read_write.c; echo $$?),0)
-      $(info -- $(REPO_NAME)/manual_hook: ksu_handle_sys_read found)
-    else
-      $(info -- You lost ksu_handle_sys_read hook in your kernel)
-      $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-      $(error You should integrate $(REPO_NAME) in your kernel.)
-    endif
+    $(eval $(call check_ksu_hook,ksu_handle_sys_read,$(srctree)/fs/read_write.c))
+
   endif
 
   ifeq ($(shell grep -q "ksu_vfs_read_hook" $(srctree)/fs/read_write.c; echo $$?),0)
@@ -57,59 +57,13 @@
   ifeq ($(CONFIG_KSU_MANUAL_HOOK_AUTO_INPUT_HOOK), y)
     $(info -- $(REPO_NAME)/manual_hook: You are using input_handler for input hooks.)
   else
-    ifeq ($(shell grep -q "ksu_handle_input_handle_event" $(srctree)/drivers/input/input.c; echo $$?),0)
-      $(info -- $(REPO_NAME)/manual_hook: ksu_handle_input_handle_event found)
-    else
-      $(info -- You lost ksu_handle_input_handle_event hook in your kernel)
-      $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-      $(error You should integrate $(REPO_NAME) in your kernel.)
-    endif
+    $(eval $(call check_ksu_hook,ksu_handle_input_handle_event,$(srctree)/drivers/input/input.c))
   endif
 
-  ifeq ($(shell grep -q "ksu_handle_execveat" $(srctree)/fs/exec.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/manual_hook: ksu_handle_execveat found)
-  else
-    $(info -- You lost ksu_handle_execveat hook in your kernel)
-    $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-    $(error You should integrate $(REPO_NAME) in your kernel.)
-  endif
+  $(eval $(call check_ksu_hook,ksu_handle_execveat,$(srctree)/fs/exec.c))
+  $(eval $(call check_ksu_hook,ksu_handle_faccessat,$(srctree)/fs/open.c))
+  $(eval $(call check_ksu_hook,ksu_handle_stat,$(srctree)/fs/stat.c))
+  $(eval $(call check_ksu_hook,ksu_handle_newfstat_ret,$(srctree)/fs/stat.c))
+  $(eval $(call check_ksu_hook,ksu_handle_fstat64_ret,$(srctree)/fs/stat.c))
+  $(eval $(call check_ksu_hook,ksu_handle_sys_reboot,$(srctree)/kernel/reboot.c))
 
-  ifeq ($(shell grep -q "ksu_handle_faccessat" $(srctree)/fs/open.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/manual_hook: ksu_handle_faccessat found)
-  else
-    $(info -- You lost ksu_handle_faccessat hook in your kernel)
-    $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-    $(error You should integrate $(REPO_NAME) in your kernel.)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_stat" $(srctree)/fs/stat.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/manual_hook: ksu_handle_stat found)
-  else
-    $(info -- You lost ksu_handle_stat hook in your kernel)
-    $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-    $(error You should integrate $(REPO_NAME) in your kernel.)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_newfstat_ret" $(srctree)/fs/stat.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/manual_hook: ksu_handle_newfstat_ret found)
-  else
-    $(info -- You lost ksu_handle_newfstat_ret hook in your kernel)
-    $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-    $(error You should integrate $(REPO_NAME) in your kernel.)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_fstat64_ret" $(srctree)/fs/stat.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/manual_hook: ksu_handle_fstat64_ret found)
-  else
-    $(info -- You lost ksu_handle_fstat64_ret hook in your kernel)
-    $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-    $(error You should integrate $(REPO_NAME) in your kernel.)
-  endif
-
-  ifeq ($(shell grep -q "ksu_handle_sys_reboot" $(srctree)/kernel/reboot.c; echo $$?),0)
-    $(info -- $(REPO_NAME)/manual_hook: ksu_handle_sys_reboot found)
-  else
-    $(info -- You lost ksu_handle_sys_reboot hook in your kernel)
-    $(info -- Read: https://resukisu.github.io/guide/manual-integrate.html)
-    $(error You should integrate $(REPO_NAME) in your kernel.)
-  endif
